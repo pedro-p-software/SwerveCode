a4ce38fc085993c24e62c72936d5e21190124f7e
pedro <pedrososter2019@gmail.com>
2026-05-22 11:39:45 -0300

    more basic setup

--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -4,10 +4,16 @@
 
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.sensors.PigeonIMU;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.math.kinematics.ChassisSpeeds;
 import edu.wpi.first.math.kinematics.Kinematics;
 import edu.wpi.first.math.kinematics.SwerveDriveKinematics;
+import edu.wpi.first.math.kinematics.SwerveDriveOdometry;
+import edu.wpi.first.math.kinematics.SwerveModulePosition;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
@@ -15,6 +21,9 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 public class SwerveDrive extends SubsystemBase {
 
   SwerveDriveKinematics kinematics;
+  SwerveDriveOdometry odometry;
+  PigeonIMU gyro;
+
   /** Creates a new SwerveDrive. */
   public SwerveDrive() {
 
@@ -29,6 +38,14 @@ public class SwerveDrive extends SubsystemBase {
       new Translation2d(Units.inchesToMeters(-10), Units.inchesToMeters(-10))
     );
 
+    gyro = new PigeonIMU(null);
+
+    odometry = new SwerveDriveOdometry(
+      kinematics,
+      null,
+      new SwerveModulePosition[] {new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition(), new SwerveModulePosition()});
+      new Pose2d(0,0, new Rotation2d());
+      
     
   }
 
