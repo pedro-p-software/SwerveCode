2f4bd3218d037126609a4677cc07e41f4490d9b7
pedro <pedrososter2019@gmail.com>
2026-05-26 11:10:15 -0300

    removal of unused imports and misc changes

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,35 +4,27 @@
 
 package frc.robot;
 
-import swervelib.SwerveDrive;
 import swervelib.SwerveInputStream;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.button.CommandPS4Controller;
 import frc.robot.Constants.OperatorConstants;
 
-/**
- * This class is where the bulk of the robot should be declared. Since Command-based is a
- * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
- * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
- * subsystems, commands, and trigger mappings) should be declared here.
- */
 public class RobotContainer {
-  // The robot's subsystems and commands are defined here...
+ 
   private final frc.robot.subsystems.SwerveDrive drivebase = new frc.robot.subsystems.SwerveDrive();
 
-  // Replace with CommandPS4Controller or CommandJoystick if needed
   private final CommandPS4Controller controle = new CommandPS4Controller(0);
 
   /** The container for the robot. Contains subsystems, OI devices, and commands. */
   public RobotContainer() {
-    // Configure the trigger bindings
+
     configureBindings();
+    drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
   }
 
   private void configureBindings() {
     
     configureBindings();
-    drivebase.setDefaultCommand(driveFieldOrientedAngularVelocity);
   }
 
   //quao rapido se move
--- src/main/java/frc/robot/subsystems/SwerveDrive.java
+++ src/main/java/frc/robot/subsystems/SwerveDrive.java
@@ -11,6 +11,8 @@ import java.util.function.Supplier;
 
 import com.ctre.phoenix6.hardware.TalonFX;
 import com.studica.frc.AHRS;
+import com.studica.frc.AHRS.NavXComType;
+import com.studica.frc.AHRS.NavXUpdateRate;
 
 import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Rotation2d;
@@ -20,7 +22,6 @@ import edu.wpi.first.wpilibj.Filesystem;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
-import swervelib.SwerveInputStream;
 import swervelib.SwerveModule;
 import swervelib.parser.SwerveParser;
 
@@ -29,17 +30,13 @@ public class SwerveDrive extends SubsystemBase {
   AHRS gyro;
   SwerveModule[] swerveModule;
 
-  TalonFX frontleft = new TalonFX(0);
-  TalonFX frontleftrotate = new TalonFX(0);
-
-
     File directory = new File(Filesystem.getDeployDirectory(),"swerve");
     
     swervelib.SwerveDrive swerveDrive;
     
-    private final Pose2d startingPose = new Pose2d(new Translation2d(Meter.of(16),
-                                                                              Meter.of(4)),
-                                                            Rotation2d.fromDegrees(180));
+    private final Pose2d startingPose = new Pose2d(new Translation2d(Meter.of(0),
+                                                                     Meter.of(0)),
+                                                            Rotation2d.fromDegrees(gyro.getAngle()));
 
 
   /** Creates a new SwerveDrive. */
@@ -49,7 +46,6 @@ public class SwerveDrive extends SubsystemBase {
     {
       swerveDrive = new SwerveParser(directory).createSwerveDrive(Constants.MAX_SPEED, startingPose);
 
-      // swerveDrive = new SwerveParser(directory).createSwerveDrive(maximumSpeed, angleConversionFactor, driveConversionFactor);
     } catch (Exception e)
     {
       throw new RuntimeException(e);
